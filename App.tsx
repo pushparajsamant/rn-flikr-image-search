d81f5189e7bb5fca56890c2f5ad495f2d63415ad
pushparajsamant <samant.pushparaj@gmail.com>
2023-07-23 21:11:52 +1000

    Added code for env

--- App.tsx
+++ App.tsx
@@ -11,10 +11,12 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import store from './redux/store';
 import {Provider} from 'react-redux';
+import Config from 'react-native-config';
 import 'react-native-gesture-handler';
 import MainNavigation from './navigation/MainNavigation';
 
 function App(): React.JSX.Element {
+  console.log(Config);
   const isDarkMode = useColorScheme() === 'dark';
   //const dispatch = useAppDispatch();
   const backgroundStyle = {
