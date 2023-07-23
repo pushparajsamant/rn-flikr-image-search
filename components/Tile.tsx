42c1b53d7821d3fbfe3ba488f4481d9d27660f64
pushparajsamant <samant.pushparaj@gmail.com>
2023-07-23 22:27:16 +1000

    Search from keyboard

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import {ImageType} from '../redux/reducers/SearchReducer';
-import {Image, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
+import FastImage from 'react-native-fast-image';
+import {ImageType} from '../redux/types';
 interface Props {
   item: ImageType;
 }
 const Tile: React.FC<Props> = ({item}) => {
   const imageURI = `https://farm${item.farm}.static.flickr.com/${item.server}/${item.id}_${item.secret}.jpg`;
   //console.log(imageURI);
-  return <Image source={{uri: imageURI}} style={styles.image} />;
+  //return <Image source={{uri: imageURI}} style={styles.image} />;
+  return (
+    <FastImage
+      style={styles.image}
+      source={{
+        uri: imageURI,
+        priority: FastImage.priority.normal,
+      }}
+      resizeMode={FastImage.resizeMode.contain}
+    />
+  );
 };
 export default Tile;
 const styles = StyleSheet.create({
